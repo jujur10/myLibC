4a9c97549f86970366aa840fd71cf9792f51d039
Julien ROIRON <julien.roiron@epitech.eu>
2024-02-15 17:23:23 +0100

    strfry with xor

--- strfry.asm
+++ strfry.asm
@@ -15,7 +15,6 @@ section .text
         xor rax, rax
         xor r10, r10
         xor r8, r8      ; Index
-        xor r9, r9
         xor rcx, rcx
         call get_length
         mov r9, rax     ; Length
@@ -26,7 +25,7 @@ section .text
 
         swap:
         mov sil, byte [rdi + rdx]       ; Value at the randomized index
-        mov cl, byte [rdi + r8]         ; Current value
+        mov cl, byte [rdi + r8] ; Current value
         mov byte [rdi + rdx], cl
         mov byte [rdi + r8], sil
         inc r8
@@ -34,13 +33,12 @@ section .text
         begin:
         rdtsc           ; Get clock (source of random)
         xor rdx, rdx    ; Clean RDX register for the remainder
+        xor rax, 0x98452; Random number
         cmp r9, r8
         je end
         mov r10, r9
         inc r10
         div r10
-        cmp rdx, 0
-        je end
         jmp swap
 
         end:
