5c58df0bf665bf24857e57d88306c243125bb997
Julien ROIRON <julien.roiron@epitech.eu>
2024-02-15 17:14:14 +0100

    syscall fixed

--- syscall.asm
+++ syscall.asm
@@ -6,13 +6,13 @@ section .text
         mov rax, rdi
         mov rdi, rsi
         mov rsi, rdx
-        mov rdx, r10
+        mov rdx, rcx
         mov r10, r8
         mov r8, r9
         mov r9, [rsp + 8]
         syscall
-        cmp rax, -1
-        jle error
+        cmp rax, -4095
+        jae error
         ret
 
         error:
