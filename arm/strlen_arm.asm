cca82a0d0dbebb2e4edd58253d0db0a189e04986
Julien ROIRON <julien.roiron@epitech.eu>
2024-02-18 17:25:58 +0100

    strlen arm

---
+++ arm/strlen_arm.asm
@@ -0,0 +1,16 @@
+global strlen_arm
+
+section .text
+        strlen_arm:
+        eor r1, r1
+
+        begin:
+        ldrb r2, [r0], #1
+        cmp r2, #0
+        beq end
+        add r1, r1, #1
+        b begin
+
+        end:
+        mov r0, r1
+        bx lr
